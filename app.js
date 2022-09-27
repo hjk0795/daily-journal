6f868671294acd418a144188c023496ec7582275
hjk0795 <hjk0795@gmail.com>
2022-09-27 17:14:40 +1300

    app.js modified

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get("/", function(req, res) {
-  res.render(__dirname+"/views/home.ejs", {Content: homeStartingContent, post: Post.find()});
+  Post.find({}, function(err, posts){
+    res.render(__dirname+"/views/home.ejs", {Content: homeStartingContent, post: posts});
+  });
 })
 
 app.get("/about", function(req, res) {
