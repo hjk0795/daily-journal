1aaa01e75507a550295a2acff53b96bd6c1c4d47
hjk0795 <hjk0795@gmail.com>
2022-09-27 17:00:02 +1300

    app.js modified

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const conf = require("dotenv").config();
 
 const app = express();
 app.set('view engine', 'ejs');
